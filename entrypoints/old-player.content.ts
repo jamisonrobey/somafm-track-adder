224b8fcab7abc0235be2282db0ddc52d7eb484b1
jamisonrobey <jamisonrobey@gmail.com>
2025-09-29 16:15:44 +1000

    refactor: getIcon into own file because used in new-player.content.ts

--- entrypoints/old-player.content.ts
+++ entrypoints/old-player.content.ts
@@ -100,17 +100,3 @@ export default defineContentScript({
     if (listBody) hookListBody(listBody);
   },
 });
-
-// https://wxt.dev/guide/essentials/assets.html#inside-content-scripts
-const getIcon = (type: "spotify" | "loading" | "checkmark" | "X") => {
-  switch (type) {
-    case "spotify":
-      return browser.runtime.getURL("/spotify-icon.svg");
-    case "loading":
-      return browser.runtime.getURL("/loading-icon.svg");
-    case "checkmark":
-      return browser.runtime.getURL("/checkmark.svg");
-    case "X":
-      return browser.runtime.getURL("/X.svg");
-  }
-};
